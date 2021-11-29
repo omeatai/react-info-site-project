d76cf216c48c9ac12a0e00557fe9febe2f22f92d
IFEANYI OMEATA <32337103+omeatai@users.noreply.github.com>
2021-11-29 18:40:50 +0100

    m

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 function App() {
-  return (
-    <div>
-      <h1>Hello Everyone!</h1>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Hello Everyone!</h1>
+		</div>
+	);
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
