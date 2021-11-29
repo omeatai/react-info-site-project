fbf80b4f2b561d6cc88fecff35f186be0025a55b
IFEANYI OMEATA <32337103+omeatai@users.noreply.github.com>
2021-11-29 19:04:17 +0100

    m

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function Tapp() {
+function App() {
 	return (
 		<div>
 			<h1>Welcome to awesome React!</h1>
+			<p>Let's get started!</p>
 		</div>
 	);
 }
 
-ReactDOM.render(<Tapp />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
